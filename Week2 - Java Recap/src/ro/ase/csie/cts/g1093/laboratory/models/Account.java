b9f69e1c9e090de0581f6b1c655c9dc0d556db52
Glodeanu Theodor <theodor.glodeanu@gmail.com>
2021-03-04 14:30:21 +0200

    Added Week 2 laboratory

--- Week2 - Java Recap/src/ro/ase/csie/cts/g1093/laboratory/models/Account.java
+++ Week2 - Java Recap/src/ro/ase/csie/cts/g1093/laboratory/models/Account.java
@@ -1,5 +1,13 @@
 package ro.ase.csie.cts.g1093.laboratory.models;
 
-public class Account {
+public abstract class Account {
+
+	public abstract double getBalance();
+
+	public abstract void deposit(Double amount);
+
+	public abstract void withdraw(Double amount);
+
+	public abstract void transfer(Account destination, Double amount);
 
 }
