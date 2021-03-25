8d6d0be7e477a1199a25e23f3e8bd6172d7261cb
Glodeanu Theodor <theodor.glodeanu@gmail.com>
2021-03-25 14:36:04 +0200

    Singleton - pre-break

---
+++ Week5 - Singleton/src/ro/ase/csie/cts/g1093/dp/singleton/PlayerModule.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.g1093.dp.singleton;
+
+public class PlayerModule {
+
+	public PlayerModule() {
+//		RESTBackend backend = new RESTBackend("acs.ase.ro/cts", "get some data");
+	}
+}
